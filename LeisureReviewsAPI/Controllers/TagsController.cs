45460d3822fcfa123ed3b8fcf6ced4bd0057bbe1
Виталий Лозюк <vitalik050405@gmail.com>
2024-01-17 18:49:03 +0300

    Added tags cloud

---
+++ LeisureReviewsAPI/Controllers/TagsController.cs
@@ -0,0 +1,20 @@
+﻿using LeisureReviewsAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeisureReviewsAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/tags")]
+    public class TagsController : BaseController
+    {
+        private readonly ITagsRepository tagsRepository;
+        public TagsController(IUsersRepository usersRepository, ITagsRepository tagsRepository) : base(usersRepository)
+        {
+            this.tagsRepository = tagsRepository;
+        }
+
+        [HttpGet("get-weights")]
+        public async Task<IActionResult> GetTagsWeights() =>
+            Ok(await tagsRepository.GetWeightsAsync());
+    }
+}
